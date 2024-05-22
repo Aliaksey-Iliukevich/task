dca3966667ef31b43e58cc917e1f8c60a40f50a1
Alexey Iliukevich <aleks.ilykevich@gmail.com>
2024-05-22 10:56:19 +0300

    feat: refactor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <SearchInput label="Input value:" v-model="modelValue" :font-size="fontSize" :input-classes="inputClasses.join(' ')" @update:value="updateInputValue" @clearInput="handleClearInput" />
+    <SearchInput 
+      label="Input value:" 
+      v-model="search"
+      class="class-1 class-2"
+      :font-size="fontSize"
+      placeholder="Найти..."
+      @clearInput="handleClearInput"
+    />
+    {{ search }}
   </div>
 </template>
 
@@ -13,17 +21,13 @@ export default {
   },
   data() {
     return {
-      modelValue: '', // Реактивное изменение значения введенного в поле поиска
+      search: '', // Реактивное изменение значения введенного в поле поиска
       fontSize: 20, // Установлен нужный размер шрифта
       inputClasses: ['class-1', 'class-2'] // Классы, передаваемые в инпут
     };
   },
   methods: {
-    updateInputValue(newValue) {
-      this.modelValue = newValue;
-    },
     handleClearInput() {
-      this.modelValue = '';
       console.log('Инпут очищен!');
     }
   }
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="input-container">
     <label>{{ label }}</label>
-    <div class="input-wrapper" :style="{ 'position': buttonPosition }">
-      <input v-model="inputValue" @input="updateParentValue" @keyup="checkWordsCount"
-             :style="{ 'fontSize': fontSize + 'px' }"
-             :class="inputClasses" />
+    <div class="input-wrapper">
+      <input 
+        v-model="innerValue"
+        v-bind="$attrs"
+      />
       <button @click="clearInput">Clear</button>
     </div>
-    <p v-if="inputValue !== null">
-      Кол-во символов: {{ charCount }}
-      <span v-if="wordsLimit" style="color: red;">Количество слов не должно превышать 3</span>
+    <p v-if="countSearch > 0">Кол-во символов: {{ countSearch }}
+      <span v-if="isLimit" style="color: red;">Количество слов не должно превышать 3</span>
     </p>
   </div>
 </template>
@@ -17,45 +17,45 @@
 <script>
 export default {
   props: {
-    fontSize: {
-      type: Number,
-      default: 16
-    },
     label: String,
-    buttonPosition: String,
     modelValue: String,
-    inputClasses: String
   },
+  emits: ['update:modelValue', 'clear'],
   data() {
     return {
-      inputValue: this.modelValue || '', 
-      wordsLimit: false
+      wordsLimit: 3, 
+      isLimit: false
     };
   },
   computed: {
-    charCount() {
-      if (this.inputValue) {
-        return this.inputValue.length;
+    innerValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    },
+    countSearch() {
+      if(this.innerValue) {
+        return this.innerValue.length;
       }
       return 0;
     }
   },
   watch: {
-    value(newValue) {
-      this.inputValue = newValue;
+    innerValue(value) {
+      this.checkWordsCount(value);
     }
   },
   methods: {
     clearInput() {
-      this.inputValue = '';
-      this.$emit('clearInput');
+      this.innerValue = '';
+      this.$emit('clear');
     },
-    updateParentValue() {
-      this.$emit('update:value', this.inputValue);
-    },
-    checkWordsCount() {
-      const words = this.inputValue.trim().split(/\s+/);
-      this.wordsLimit = words.length > 3;
+    checkWordsCount(value) {
+      const wordsCount = value?.trim().split(/\s+/).length;
+      this.isLimit = wordsCount > this.wordsLimit;
     }
   }
 };
